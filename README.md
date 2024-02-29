fa37a1ae8a686670f5f130eb4cbf003571afdb39
Yurii Protsenko <elsoul@gmail.com>
2024-02-29 13:32:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Generate descriptions from OpenAI
+Generate descriptions for property items. 
+Source data gets from variable "in_file" in csv.
+Output file from "out_file"
+
+## Content
+- All relations in pyproject.toml
+- All starting prompts in conf.py
+- You need to create .env and put there api-key for openAI API
+
+## Advice
+You should rename column E in source file to "Type property"
---
+++ main.py
@@ -0,0 +1,117 @@
+import os
+import csv
+import openai
+from time import time
+from pathlib import Path
+from conf import prompt_desc, structure, finish_detail
+
+
+# from openai import OpenAI
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+prompt = openai.OpenAI(api_key=OPENAI_API_KEY)
+prompt_chat_id = None
+description = openai.OpenAI(api_key=OPENAI_API_KEY)
+desc_chat_id = None
+# thread = client.beta.threads.create()
+
+
+class RequestToAI:
+    def make_request(self, chat, content: str):
+        response = chat.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an excellent copyrighter.",
+                },
+                {
+                    "role": "user",
+                    "content": content,
+                },
+            ],
+        )
+        return response
+
+
+def sanitize(row_str: str) -> str:
+    if row_str:
+        return row_str.replace("\t", "").strip().replace("\r", "")
+    else:
+        return row_str
+
+
+in_file = Path("1000.csv")
+out_file = Path("1000 descriptions.csv")
+request = RequestToAI()
+fields_read = [
+    "\ufeffСсылка",
+    "H1",
+    "City",
+    "Area",
+    "Type property",
+    "Number of Bedrooms",
+    "Square",
+    "Price in AED",
+    "Address",
+    "To the sea",
+    "To the center",
+    "Due date",
+    "Description",
+    "Rent",
+    "categoriya po komnatam",
+    "Type",
+    "Location",
+    "Peculiarities",
+    "Internal infrastructure",
+    "External infrastructure",
+    "",
+]
+fields_write = fields_read.copy()
+fields_write.append("response")
+
+start_time = time()
+
+
+def main():
+    counter = 0
+    with open(out_file, "w", newline="", encoding="utf-8") as fo:
+        writer = csv.DictWriter(
+            fo, fieldnames=fields_write, delimiter=";", lineterminator="\r"
+        )
+        writer.writeheader()
+        with open(in_file, "r", newline="", encoding="utf-8") as fi:
+            while True:
+                reader = csv.DictReader(fi, delimiter=";")
+                for row in reader:
+                    counter += 1
+                    if counter == 5:
+                        break
+                    cur_time = time()
+                    for k in fields_read:
+                        row[k] = sanitize(row[k])
+
+                    parameters = "Характеристики объекта недвижимости: "
+                    for i in range(2, len(fields_read)):
+                        parameters += f"{fields_read[i]} - {row[fields_read[i]]}, "
+
+                    prompt_result = request.make_request(prompt, prompt_desc)
+
+                    prompt_description = prompt_result.choices[0].message.content
+
+                    content = (
+                        f"{prompt_description} {structure} {parameters} {finish_detail}"
+                    )
+
+                    completion = request.make_request(description, content)
+                    result = completion.choices[0].message.content.replace("\n", "")
+
+                    row.update({"response": f"\t{result}"})
+                    writer.writerow(row)
+
+                    print(f"Duration {time()-cur_time}")
+                break
+    print(f"Process was working {time()-start_time}")
+
+
+if __name__ == "__main":
+    main()
